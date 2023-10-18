c073de53fd572f4a54eed32cb47ab082543e4ebd
Izuchukwu Mba <izuchukwumba100@gmail.com>
2023-10-18 17:06:00 -0500

    Changes 7

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 // START SERVER
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
